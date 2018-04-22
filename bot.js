41c828064507f058cec08e2d8ae212fb4433dcf6
SrTruxX <38599626+SrTruxX@users.noreply.github.com>
2018-04-22 18:40:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.login(process.env.BOT_TOKEN);
+
 client.on('ready', () => {
-    console.log('I am ready!');
+    client.user.setGame('Viado')
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+    let msg = message.content.toUpperCase();
+    let sender = message.author;
+    let cont = message.content.slice(prefix.length).split(" ");
+    let args = cont.slice(1);
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+    if(msg.startsWith(prefix + 'KICK') || msg.startsWith(prefix + 'EXPULSAR')){
+
+        message.reply('Viado');
+
+    }
+
+});
