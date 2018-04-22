a1f3ba66af7e0a804b2b6dcbefaaa162779a50c6
SrTruxX <38599626+SrTruxX@users.noreply.github.com>
2018-04-22 02:25:11 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Estou vivo!');
 });
 
 client.login(process.env.BOT_TOKEN);
